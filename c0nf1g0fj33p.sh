596d80a1832c0dcac2e56b80cdc63249e66fe4fa
russseaman <38870276+russseaman@users.noreply.github.com>
2018-08-20 16:28:38 -0400

    adding programs to the mix

--- c0nf1g0fj33p.sh
+++ c0nf1g0fj33p.sh
@@ -3,7 +3,7 @@
 #j33p's auto-setup script
 
 #Programs to install
-declare -a toInstall=('git' 'xorg' 'neofetch' 'task' 'curl' 'wget' 'vim' 'tmux' 'st' 'wifish' 'neomutt' 'zsh' 'alsa' 'bspwm' 'sxhkd' 'qutebrowser')
+declare -a toInstall=('git' 'xorg' 'neofetch' 'task' 'curl' 'wget' 'vim' 'tmux' 'st' 'wifish' 'neomutt' 'zsh' 'alsa' 'bspwm' 'sxhkd' 'qutebrowser' 'rofi' 'dmenu' 'xfce4-terminal')
 
 #File Dirs to make for home
 declare -a mkDirs=('Documents' 'Downloads' 'Pictures' 'Music 'Movies')
