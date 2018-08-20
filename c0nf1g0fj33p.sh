68c9dae8f4696a4f2b6ded10ee39be9351d5e3c1
russseaman <38870276+russseaman@users.noreply.github.com>
2018-08-20 16:01:08 -0400

    updating the files manually

--- c0nf1g0fj33p.sh
+++ c0nf1g0fj33p.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 #j33p's auto-setup script
-declare -a toInstall=('git' 'xorg' 'i3' 'neofetch' 'task' 'curl' 'wget')
-
+#
+#Programs to install
+declare -a toInstall=('git' 'xorg' 'i3-gaps' 'neofetch' 'task' 'curl' 'wget' 'vim' 'tmux' 'st' 'wifish' 'neomutt' 'zsh' 'i3blocks' 'alsa')
+#
+#File Dirs to make for home
+declare -a mkDirs=('Documents' 'Downloads' 'Pictures' 'Music 'Movies')
 #sync mirrors
 sudo xbps-install  -y -S
 #update packages
 sudo xbps-install -y -Su
 
+#Program install loop
 for i in "${toInstall[@]}" 
 do
 	sudo xbps-install -y $i	
 
 done	
+
+#Make Dirs loop
+cd ..		#Go up a directory level
+for i in "${mkDirs[@]}"
+do
+	sudo mkdir $i
+done
+
+#clean up, after intial install delete the old install script/dir
+sudo rm -rf v01dr1c3/
+
 sudo xbps-install -y -Su
