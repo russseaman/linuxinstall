996154c5a5373b4e9f2a9038e15ac8166de2ce6f
russseaman <russ_seaman@mac.com>
2019-04-16 09:45:06 -0400

    updating for void...init tests

--- lvercheck.sh
+++ lvercheck.sh
@@ -2,19 +2,24 @@
 distro=$(lsb_release -si)
 version=$(lsb_release -sr)
 
-echo "Distro is $distro"
-if [ "$distro" == "Ubuntu" ]
-then
-    case $version in
-        16.04)
-            echo "Ubuntu" = "16.04"
-            pkmgr="apt-get"
-            echo "Package manager = $pkmgr"
-            ;;
-        18.04)
-            echo "Ubuntu = 18.04"
-            pkmgr="apt"
-            echo "Package manager = $pkmgr"
-            ;;
-    esac
-fi
+
+case $distro in
+   "Void")
+        echo "Void Linux Detected"
+        pkmgr="xbps-install"
+        ;;
+   "Ubuntu")
+        echo "Ubuntu Linux Detected"
+        case $version in
+            16.04)
+                echo "Ubuntu" = "16.04"
+                pkmgr="apt-get"
+                echo "Package manager = $pkmgr"
+                ;;
+            18.04)
+                echo "Ubuntu = 18.04"
+                pkmgr="apt"
+                echo "Package manager = $pkmgr"
+                ;;
+        esac
+esac
